e8c1e5db025cc244f19ea7de70b4ff68a8a555b5
Anshul Khandelwal <kanshul45@gmail.com>
2023-10-30 11:51:49 -0700

    accept and reject buttons

--- Frontend/src/pages/index.js
+++ Frontend/src/pages/index.js
@@ -1,8 +1,10 @@
 import React, { useState } from 'react';
 
+
 export default function Home() {
   const [text, setText] = useState('');
-  const [url, setUrl] = useState(''); // To store the URL
+  const [url, setUrl] = useState('');
+  const [isImageAccepted, setImageAccepted] = useState(null); // null = not decided, true = accepted, false = rejected
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -22,12 +24,23 @@ export default function Home() {
 
       if (data.url) {
         setUrl(data.url);
+        setImageAccepted(null); // Reset the decision state when a new image is fetched
       } else {
         console.error('Failed to get URL.');
       }
     } catch (error) {
       console.error('Error:', error);
     }
+  };
+
+  const handleAccept = () => {
+    setImageAccepted(true);
+    // You can add any additional logic here for when the image is accepted
+  };
+
+  const handleReject = () => {
+    setImageAccepted(false);
+    setUrl(''); // Clear the image if rejected, or add any other logic if needed
   };
 
   return (
@@ -45,11 +58,22 @@ export default function Home() {
         </button>
       </form>
 
-      {url && (
+      {url && isImageAccepted === null && (
         <div className="mt-8">
           <img className="rounded-lg shadow" src={url} alt="Generated from input" />
+          <div className="mt-4 flex gap-4">
+            <button onClick={handleAccept} className="bg-green-500 text-white p-2 rounded hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-green-500">
+              Accept
+            </button>
+            <button onClick={handleReject} className="bg-red-500 text-white p-2 rounded hover:bg-red-600 focus:outline-none focus:ring-2 focus:ring-red-500">
+              Reject
+            </button>
+          </div>
         </div>
       )}
+
+      {isImageAccepted === true && <p className="mt-8 text-green-500">Image accepted!</p>}
+      {isImageAccepted === false && <p className="mt-8 text-red-500">Image rejected.</p>}
     </div>
   );
 }
