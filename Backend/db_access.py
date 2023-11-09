1e0ab153b3304c953d0846c708a93a900d19b346
nnayk <79224511+nnayk@users.noreply.github.com>
2023-11-08 17:20:17 -0800

    Add proper request handling for create_user endpoint

--- Backend/db_access.py
+++ Backend/db_access.py
@@ -1,5 +1,13 @@
 from flask import Flask, jsonify, request
-from mongoengine import connect, Document, StringField, IntField, ReferenceField, NotUniqueError
+from mongoengine import (
+    connect,
+    Document,
+    StringField,
+    IntField,
+    ReferenceField,
+    NotUniqueError,
+)
+import json
 
 from werkzeug.security import check_password_hash
 import secrets
@@ -9,86 +17,111 @@ from mongoengine.errors import DoesNotExist
 app = Flask(__name__)
 
 # MongoDB connection
-connect(db='dbPicturePerfect', host='localhost', port=27017)
+connect(db="dbPicturePerfect", host="localhost", port=27017)
+
 
 class User(Document):
     username = StringField(required=True, unique=True)
     name = StringField(required=True)
-    encrypted_password = StringField(required=True) # Assuming you're storing it as a String for now
+    encrypted_password = StringField(
+        required=True
+    )  # Assuming you're storing it as a String for now
     ranking = IntField()
 
-    meta = {'collection': 'users'}
+    meta = {"collection": "users"}
 
-class Image(Document): 
+
+class Image(Document):
     creator = ReferenceField(User, required=True)
     url = StringField(required=True)
     prompt = StringField(required=True)
     votes = IntField(default=0)
 
-    meta = {'collection': 'images'}
+    meta = {"collection": "images"}
 
 
-
-
-@app.route('/create_user', methods=['POST'])
+@app.route("/create_user", methods=["POST"])
 def create_user():
-    data = request.get_json()
-
+    data = json.loads(request.data.decode("utf-8"))
+    print(data)
     user = User(
-        username=data['username'],
-        encrypted_password=data['password'],  
-        name=data['name']
+        username=data["username"],
+        encrypted_password=data["password"],
+        name=data["name"],
     )
     try:
         user.save()
-        return jsonify({"message": "User created successfully!", "user_id": str(user.id)}), 201
+        return (
+            jsonify(
+                {
+                    "message": "User created successfully!",
+                    "user_id": str(user.id),
+                }
+            ),
+            201,
+        )
     except NotUniqueError:
-        return jsonify({"error": "Username already exists. Choose another."}), 400
+        return (
+            jsonify({"error": "Username already exists. Choose another."}),
+            400,
+        )
 
 
-@app.route('/create_image', methods=['POST'])  
+@app.route("/create_image", methods=["POST"])
 def create_image():
-    
     data = request.get_json()
 
     # Retrieve the user by ObjectId
-    creator = User.objects.get(id=data['creator'])
-    
+    creator = User.objects.get(id=data["creator"])
+
     # Associate the image with the user and save it
-    image = Image(
-        creator=creator,
-        prompt=data['prompt'],
-        url=data['url']
-    )
+    image = Image(creator=creator, prompt=data["prompt"], url=data["url"])
     image.save()
 
-    return jsonify({"message": "Image data added successfully!", "image_id": str(image.id)}), 201
+    return (
+        jsonify(
+            {
+                "message": "Image data added successfully!",
+                "image_id": str(image.id),
+            }
+        ),
+        201,
+    )
 
 
-
-
-@app.route('/users')
+@app.route("/users")
 def get_users():
     users = User.objects.all()
-    return jsonify([{
-        'username': user.username,
-        'name': user.name,
-        'user_id': str(user.id) 
-    } for user in users])
+    return jsonify(
+        [
+            {
+                "username": user.username,
+                "name": user.name,
+                "user_id": str(user.id),
+            }
+            for user in users
+        ]
+    )
 
-@app.route('/images')
+
+@app.route("/images")
 def get_images():
     images = Image.objects.all()
-    return jsonify([{
-        'creator_id': str(image.creator.id),  # updated from user to creator
-        'prompt': image.prompt,
-        'url': image.url,
-        'votes': image.votes,
-        'image_id': str(image.id)  
-    } for image in images])
-
-
+    return jsonify(
+        [
+            {
+                "creator_id": str(
+                    image.creator.id
+                ),  # updated from user to creator
+                "prompt": image.prompt,
+                "url": image.url,
+                "votes": image.votes,
+                "image_id": str(image.id),
+            }
+            for image in images
+        ]
+    )
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5001, debug=True)
+    app.run(host="0.0.0.0", port=5001, debug=True)
